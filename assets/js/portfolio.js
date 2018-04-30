33faf3503a2d9c682deae06ca5a35a3af9851abe
Kate Birmingham <kabi6686@colorado.edu>
2018-04-29 22:15:26 -0600

    styled pages, added animation

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -5,6 +5,20 @@ $(function() {
 
 	$("#year").text(year);
 
+	// Click connect button, modal pops up
+	$(".connect").on("click", function() {
+		// $(".modal-connect").modal("show");
+		const buttonId = $(this).attr('id');
+  		$('#modal-container').removeAttr('class').addClass(buttonId);
+ 		$('body').addClass('modal-active');
+	})
+
+	// Click outside modal, exit out
+	$('#modal-container').click(function(){
+  		$(this).addClass('out');
+  		$('body').removeClass('modal-active');
+	})
+
 	// const colorChanger = document.getElementById("color-changer");
 	// const colors = ["lightblue", "purple", "darkblue", "blue", "green", "lightgreen", "yellow", "orange", "grey"];
 	// let counter = 0;
@@ -31,71 +45,62 @@ $(function() {
 	  [255,0,255],
 	  [255,128,0]);
 
-var step = 0;
-//color table indices for: 
-// current color left
-// next color left
-// current color right
-// next color right
-var colorIndices = [0,1,2,3];
+	var step = 0;
+	//color table indices for: 
+	// current color left
+	// next color left
+	// current color right
+	// next color right
+	var colorIndices = [0,1,2,3];
 
-//transition speed
-var gradientSpeed = 0.002;
+	//transition speed
+	var gradientSpeed = 0.002;
 
-function updateGradient()
-{
-  
-  if ( $===undefined ) return;
-  
-var c0_0 = colors[colorIndices[0]];
-var c0_1 = colors[colorIndices[1]];
-var c1_0 = colors[colorIndices[2]];
-var c1_1 = colors[colorIndices[3]];
+	function updateGradient()
+	{
+	  
+	  if ( $===undefined ) return;
+	  
+	var c0_0 = colors[colorIndices[0]];
+	var c0_1 = colors[colorIndices[1]];
+	var c1_0 = colors[colorIndices[2]];
+	var c1_1 = colors[colorIndices[3]];
 
-var istep = 1 - step;
-var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-var color1 = "rgb("+r1+","+g1+","+b1+")";
+	var istep = 1 - step;
+	var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+	var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+	var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+	var color1 = "rgb("+r1+","+g1+","+b1+")";
 
-var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-var color2 = "rgb("+r2+","+g2+","+b2+")";
+	var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+	var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+	var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+	var color2 = "rgb("+r2+","+g2+","+b2+")";
 
- $('#gradient').css({
-   background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-    background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
-  
-  step += gradientSpeed;
-  if ( step >= 1 )
-  {
-    step %= 1;
-    colorIndices[0] = colorIndices[1];
-    colorIndices[2] = colorIndices[3];
-    
-    //pick two new target color indices
-    //do not pick the same as the current one
-    colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-    colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-    
-  }
-}
+	 $('#gradient').css({
+	   background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+	    background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+	  
+	  step += gradientSpeed;
+	  if ( step >= 1 )
+	  {
+	    step %= 1;
+	    colorIndices[0] = colorIndices[1];
+	    colorIndices[2] = colorIndices[3];
+	    
+	    //pick two new target color indices
+	    //do not pick the same as the current one
+	    colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+	    colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+	    
+	  }
+	}
 
-setInterval(updateGradient,10);
+	//changes gradient on home page
+	setInterval(updateGradient,10);
 
-	// Click connect button, modal pops up
-	$(".connect").on("click", function() {
-		// $(".modal-connect").modal("show");
-		const buttonId = $(this).attr('id');
-  		$('#modal-container').removeAttr('class').addClass(buttonId);
- 		$('body').addClass('modal-active');
-	})
-
-	// Click outside modal, exit out
-	$('#modal-container').click(function(){
-  		$(this).addClass('out');
-  		$('body').removeClass('modal-active');
+	$("#dropDownMenu li").each(function(i) {
+		$(this).delay(100 * i).fadeIn(500);
 	})
 
 
