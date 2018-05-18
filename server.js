5ec254beba496506c97f5690d4073d6e41739912
Kate Birmingham <kabi6686@colorado.edu>
2018-05-18 09:17:20 -0600

    deleted favicon

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-var favicon = require("express-favicon");
 
 // Sets up the Express App
 // =============================================================
 
 // Tells node that we are creating an "express" server
 var app = express();
-
-//tells node where to access favicon
-// app.use(favicon(__dirname + '/public/assets/images/favicon.png'));
 
 // Sets an initial port
 var PORT = process.env.PORT || 3000;
