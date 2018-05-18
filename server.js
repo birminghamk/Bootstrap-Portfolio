2cab3ead669b37e44f8821d35c790e366f26104c
Kate Birmingham <kabi6686@colorado.edu>
2018-05-18 08:59:56 -0600

    uninstalled body-parser, installed express-favicon, added favicon to server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-var bodyParser = require("body-parser");
+var favicon = require("express-favicon");
 
 // Sets up the Express App
 // =============================================================
@@ -9,13 +9,11 @@ var bodyParser = require("body-parser");
 // Tells node that we are creating an "express" server
 var app = express();
 
+//tells node where to access favicon
+app.use(favicon(__dirname + '/public/assets/images/favicon.png'));
+
 // Sets an initial port
 var PORT = process.env.PORT || 3000;
-
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/JSON
-app.use(bodyParser.json());
 
 // Static directory
 app.use(express.static(__dirname + '/public'));
