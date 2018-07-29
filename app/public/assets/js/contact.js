07679d07e387a920a523048f8b362552b7f8fb7f
Kate Birmingham <kabi6686@colorado.edu>
2018-07-29 12:40:11 -0600

    added contact form/styles

--- app/public/assets/js/contact.js
+++ app/public/assets/js/contact.js
@@ -1,3 +1,15 @@
 $(function() {
     $('.navbar').hide().delay(50).slideDown();
+    
+    const name = $("#name");
+    const email = $("#email");
+    const message = $("#message");
+
+    $("#submit").on("click", (e) => {
+        
+    });
+    
+
+
 });
+
