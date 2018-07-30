6b480aea9a1d84e355e251641a7c51434e3f7085
Kate Birmingham <kabi6686@colorado.edu>
2018-07-30 17:10:37 -0600

    added navbar animation contact page

--- app/public/assets/js/contact.js
+++ app/public/assets/js/contact.js
@@ -1,34 +1,5 @@
-// $(function() {
-//     $('.navbar').hide().delay(50).slideDown();
+$(function() {
+    $('.navbar').hide().delay(50).slideDown();
 
-//     //get user form input
-//     const nameInput = $("#name").val().trim();
-//     const emailInput = $("#email").val().trim();
-//     const messageInput = $("#message").val().trim();
-
-//     //mailgun.js
-//     const mailgun = require('mailgun.js');
-//     const key = '';
-//     const mg = mailgun.client({
-//         username: '',
-//         key: key
-//     });
-
-//     function sendMessage() {
-//         mg.messages.create('', {
-//             from: `${nameInput} ${emailInput}`,
-//             to: ["kate.birmingham212@gmail.com"],
-//             subject: "New User Message",
-//             text: messageInput
-//         })
-//         .then(msg=> console.log(msg)) //logs response data
-//         .catch(err => console.log(err)); //logs any error
-//     }
-
-//     //click submit button
-//     $("#submit").on("click", () => {
-//         sendMessage();
-//     });
-
-// });
+});
 
