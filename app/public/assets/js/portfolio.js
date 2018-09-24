8cf342b19ad754fb5cbaddb55ee9f2e922bb3f65
Kate Birmingham <kabi6686@colorado.edu>
2018-09-24 13:30:58 -0600

    reworking design on about page, edited text on contact page, fixed media queries on portfolio page, changed bio on home page

--- app/public/assets/js/portfolio.js
+++ app/public/assets/js/portfolio.js
@@ -1,15 +1,18 @@
 $(function() {
 	var $projectLi = $("#projectMenu li");
-	$projectLi.hide();
-	$(".buildSchema").hide().slideDown();
+	function onPageLoad() {
+		$projectLi.hide();
+		$(".buildSchema").hide().slideDown();
+	}
+
+	onPageLoad();
 
 	//fade in animation for project list
 	function projectAnimation() {
 		$projectLi.hide().each(function(i) {
 			$(this).delay(100 * i).fadeIn(5000);
 		});
-		// console.log("list animation");
 	};
 
-	setTimeout(projectAnimation, 700);
+	setTimeout(projectAnimation, 500);
 });
