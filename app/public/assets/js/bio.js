6ef81b013b4d9216ce7abca1dc8ac420ba5eb83e
Kate Birmingham <kate.birmingham@influence.tv>
2019-04-26 15:22:45 -0600

    commented out unused javascript in bio.js, text edits on bio page

--- app/public/assets/js/bio.js
+++ app/public/assets/js/bio.js
@@ -6,15 +6,15 @@ $(function() {
 
 	$('.navbar').hide().delay(50).slideDown();
 
-	//changes text color of github link on bio page
-	function changeColor () {
-		if (counter >= textcolors.length) {
-			counter = 0;
-		}
-		colorChanger.style.color = textcolors[counter];
-		counter++;
-	}
+	// //changes text color of github link on bio page
+	// function changeColor () {
+	// 	if (counter >= textcolors.length) {
+	// 		counter = 0;
+	// 	}
+	// 	colorChanger.style.color = textcolors[counter];
+	// 	counter++;
+	// }
 
-	setInterval(changeColor, 5000);
+	// setInterval(changeColor, 5000);
 
 });
