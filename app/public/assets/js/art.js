e9da079e5383231b04bd6d14e9df094023c6b426
Kate Birmingham <kate.birmingham@influence.tv>
2019-10-07 17:55:52 -0600

    added instagram link, added black and white images and lady in red

--- app/public/assets/js/art.js
+++ app/public/assets/js/art.js
@@ -1,6 +1,7 @@
 $(function() {
 	function onPageLoad() {
 		$(".colorImages").hide();
+		$(".blackandwhite").hide();
 	}
 
 	onPageLoad();
@@ -10,11 +11,19 @@ $(function() {
 		$("#cocktails").click(function(){
 			$(".cocktailIllustration").show();
 			$(".colorImages").hide();
+			$(".blackandwhite").hide();
 		  });
 
 		  $("#colorStudies").click(function(){
 			$(".cocktailIllustration").hide();
 			$(".colorImages").show();
+			$(".blackandwhite").hide();
+		  });
+		  $("#blackAndwhite").click(function(){
+			$(".blackandwhite").show();
+			$(".colorImages").hide();
+			$(".cocktailIllustration").hide();
+
 		  });
 	}
 
