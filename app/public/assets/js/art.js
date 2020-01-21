a161c7f70f10e229d10c6adccdece65123dd993c
Kate Birmingham <kate.birmingham@influence.tv>
2020-01-20 17:38:59 -0700

    added art modal for art images and reorganized art page

--- app/public/assets/js/art.js
+++ app/public/assets/js/art.js
@@ -87,4 +87,32 @@ $(function() {
 	}
 
 	artNav();
+
+	function artModal() {
+		// Get the moda
+
+		$("img").click(function() {
+			var modal = document.getElementById("artModal");
+			var id = $(this).attr("id");
+			var img = document.getElementById(id);
+			// Get the image and insert it inside the modal - use its "alt" text as a caption
+			var modalImg = document.getElementById("img01");
+			var captionText = document.getElementById("caption");
+			img.onclick = function(){
+			modal.style.display = "block";
+			modalImg.src = this.src;
+			captionText.innerHTML = this.alt;
+			}
+			// Get the <span> element that closes the modal
+			var span = document.getElementsByClassName("close")[0];
+
+			// When the user clicks on <span> (x), close the modal
+			span.onclick = function() { 
+			modal.style.display = "none";
+			}
+		})
+
+	}
+
+	artModal();
 });
