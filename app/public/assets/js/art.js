d59049cfc06416230d2e43f4096637d302436fd3
Kate Birmingham <kate.birmingham@influence.tv>
2020-01-28 12:37:20 -0700

    edited artModal() function for touchscreens

--- app/public/assets/js/art.js
+++ app/public/assets/js/art.js
@@ -89,7 +89,7 @@ $(function() {
 	artNav();
 
 	function artModal() {
-		// Get the moda
+		// Get the modal
 
 		$("img").on('click touchstart', function() {
 			var modal = document.getElementById("artModal");
@@ -98,23 +98,23 @@ $(function() {
 			// Get the image and insert it inside the modal - use its "alt" text as a caption
 			var modalImg = document.getElementById("img01");
 			var captionText = document.getElementById("caption");
-			img.onclick = function() {
-			modal.style.display = "block";
-			modalImg.src = this.src;
-			captionText.innerHTML = this.alt;
-			}
-			img.ontouchstart = function() {
+			// img.onclick = function() {
+			// modal.style.display = "block";
+			// modalImg.src = this.src;
+			// captionText.innerHTML = this.alt;
+			// }
+			img.on('click touchend', function()  {
 				modal.style.display = "block";
 				modalImg.src = this.src;
 				captionText.innerHTML = this.alt;
-			}
+			})
 			// Get the <span> element that closes the modal
 			var span = document.getElementsByClassName("close")[0];
 
 			// When the user clicks on <span> (x), close the modal
-			span.onclick = function() { 
+			span.on('click touchstart', function() {
 			modal.style.display = "none";
-			}
+			})
 		})
 
 	}
