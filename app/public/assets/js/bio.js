f2a05b715558dbea80b24a0d57814914091a36c4
Kate Birmingham <kate.birmingham@influence.tv>
2021-01-04 14:02:34 -0700

    changed colors for color changer

--- app/public/assets/js/bio.js
+++ app/public/assets/js/bio.js
@@ -1,7 +1,7 @@
 $(function() {
 
 	var colorChanger = document.getElementById("color-changer");
-	var textcolors = ["lightblue", "purple", "darkblue", "pink"];
+	var textcolors = ["rgb(226,174,205)", "rgb(188,226,181)", "rgb(154,203,232)"];
 	var counter = 0;
 
 	//changes text color of github link on bio page
